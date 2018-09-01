29665f66091de2e3a5a2366a91919f91b1f4946f
Myriam41 <lieninformatique9@gmail.com>
2018-09-01 23:03:05 +0200

    layoud homeview and postListView

--- src/View/template/default.php
+++ src/View/template/default.php
@@ -28,7 +28,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
   <div class="container">
-    <a class="navbar-brand" href="index.php?page=home"><img src="../img/stamp_JauBleu200.png" alt="logo Stamp Development"/></a>
+    <a class="navbar-brand" href="index.php?page=home"><img src="../img/stamp100.png" alt="logo Stamp Development"/></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 
     <!--Menu -->
